17ff920228ea8ecc98928f3938c8e125c24b44f1
zhujinabc <zhujinabc@163.com>
2018-09-02 18:59:20 +0800

    添加pm2配制文件

--- server/server.js
+++ server/server.js
@@ -41,7 +41,8 @@ app.use(function (error, req, res, next) {
   console.log(error)
   res.status(500).send(error)
 })
-
-app.listen(3333, function () {
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 3333
+app.listen(port, host, function () {
   console.log('Server is running on port 3333')
 })
